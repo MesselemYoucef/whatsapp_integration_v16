56a40e7197e946b88107f00121f93c447023cb7e
MesselemYoucef <messelemy@gmail.com>
2023-01-03 22:27:06 +0400

    url action created

--- models/whatsapp_sale.py
+++ models/whatsapp_sale.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 
 class WhatsappSale(models.Model):
@@ -7,3 +8,12 @@ class WhatsappSale(models.Model):
     partner_id = fields.Many2one("res.partner")
     partner_mobile = fields.Char(related="partner_id.mobile")
 
+    def whatsapp_customer(self):
+        if not self.partner_mobile:
+            raise ValidationError('Customer has no mobile phone')
+        message_whatsapp = "https://wa.me/%s?text=%s" % (self.partner_mobile, 'hello this is the message')
+        return {
+            'type': 'ir.actions.act_url',
+            'url': message_whatsapp,
+            'target': 'new'
+        }
