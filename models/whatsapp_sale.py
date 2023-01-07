c93d994ac6b99c880e9bd52d88413ae4da51dfce
MesselemYoucef <messelemy@gmail.com>
2023-01-07 22:39:58 +0400

    Fixed the sales order link via whatsapp and added options according to the sales order state

--- models/whatsapp_sale.py
+++ models/whatsapp_sale.py
@@ -11,8 +11,28 @@ class WhatsappSale(models.Model):
     def whatsapp_customer(self):
         if not self.partner_mobile:
             raise ValidationError('Customer has no mobile phone')
-        message = "Dear client please check your invoice and approve it:"
-        message_whatsapp = "https://wa.me/%s?text=%s" % (self.partner_mobile, 'hello this is the message')
+        if not self.access_token:
+            raise ValidationError('Please send by email first')
+        server_name = ""
+        try:
+            server_name = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+        except:
+            server_name = "https://www.google.com"
+
+        phrase = ""
+        if self.state == 'draft' or self.state == 'sent':
+            phrase = 'Dear client please check the draft of your invoice and confirm it.'
+        elif self.state == 'sale':
+            phrase = 'Dear client your sales order has been created.'
+        elif self.state == 'cancel':
+            phrase = 'Dear client your sales order has been canceled.'
+        else:
+            phrase = 'Please ignore this link'
+
+        message = phrase + "%0a" + server_name + self.access_url + "?access_token=" + self.access_token
+
+        message_whatsapp = "https://wa.me/%s?text=%s" % (self.partner_mobile, message)
+
         return {
             'type': 'ir.actions.act_url',
             'url': message_whatsapp,
